c7dad474fef15f36b0a18b761f3a924c9a57cd77
Mike Straw <mike.straw@automattic.com>
2021-01-06 06:58:25 -0500

    Initial commit

---
+++ routes/cards.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const router = new express.Router();
+
+const {data } = require ('../data/flashCardData.json');
+const { cards } = data;
+
+router.get('/', ( req, res) => {
+    const id = Math.floor(Math.random() * cards.length);
+
+    res.redirect(`/cards/${id}`);
+});
+
+router.get('/:id', (req, res) => {
+    const { side } = req.query;
+    const { id } = req.params;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+
+    const templateData = { 
+        text,
+        side
+    };
+
+    const  renderCard = ( flipSide ) => {
+        templateData.flip = flipSide;
+        res.render('card', templateData );
+    };
+
+    if ( side === 'question' ) {
+        templateData.hint = hint;
+        renderCard( 'answer' );
+    } else if ( side === 'answer' ) {
+        renderCard( 'question' );
+    } else {
+        res.redirect('?side=question');
+    }
+});
+
+module.exports = router;
